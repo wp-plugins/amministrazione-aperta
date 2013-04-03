5cc8634f5911165e8338fd68c63b80eb5d43e32d
Milmor <Milmor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-03 13:45:50 +0000

    nuova versione
    git-svn-id: https://plugins.svn.wordpress.org/amministrazione-aperta/trunk@691189 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- amministrazioneaperta.php
+++ amministrazioneaperta.php
@@ -3,7 +3,7 @@
 Plugin Name: Amministrazione Aperta
 Plugin URI: http://www.comune.sanpellegrinoterme.bg.it/
 Description: In materia di trasparenza nella pubblica amministrazione, il D.L. 83/2012 impone alle PA di pubblicare in un’area predisposta informazioni relative a ogni spesa effettuata che superi i 1'000 euro d
-Version: 1.1
+Version: 1.2
 Author: Marco Milesi
 Author Email: milesimarco@outlook.com
 License:
