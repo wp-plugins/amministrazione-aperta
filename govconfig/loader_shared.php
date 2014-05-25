03bdf7241ec5bb2fee17d922ee19ee755cf4c99c
Milmor <Milmor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-25 20:20:13 +0000

    version 3r2
    git-svn-id: https://plugins.svn.wordpress.org/amministrazione-aperta/trunk@920827 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- govconfig/loader_shared.php
+++ govconfig/loader_shared.php
@@ -4,7 +4,7 @@ add_action( 'admin_menu', 'register_aa_wpgov_menu_page' );
 
 function register_aa_wpgov_menu_page(){
 	if (is_plugin_active( 'amministrazione-trasparente/amministrazionetrasparente.php' ) || is_plugin_active( 'avcp/avcp.php' )) { return; }
-    add_menu_page('Impostazioni soluzioni WPGOV.IT', 'WPGov.it', 'manage_options', 'impostazioni-wpgov', 'at_wpgov_settings', 'dashicons-share-alt', 40);
+    add_menu_page('Impostazioni soluzioni WPGOV.IT', 'WPGov.it', 'manage_options', 'impostazioni-wpgov', 'aa_wpgov_settings', 'dashicons-share-alt', 40);
 }
 
 function aa_wpgov_settings () {
